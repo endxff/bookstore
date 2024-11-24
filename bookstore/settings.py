5dda661870f909e3edee6d9f6561cb4d4c6814bb
Endriw <endriwxavier@gmail.com>
2024-11-24 17:49:13 -0300

    chore: configure DRF Pagination

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -130,3 +130,8 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 INTERNAL_IPS = ["127.0.0.1"]
+
+REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
+}
